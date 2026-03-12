dcc3891e1d4f0c25198b4cb18bd950145828dfd4
stephengold <sgold@sonic.net>
2026-03-12 16:30:53 -0700

    buildscripts:  comment and whitespace

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
     alias(libs.plugins.android.application) apply false
 }
+
 configurations.configureEach {
     resolutionStrategy.cacheChangingModulesFor(0, "seconds") // to disable caching of snapshots
 }
